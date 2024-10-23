4594aab10e501c611a46ce877055f7ca97b9bc81
Weng Xuetian <wengxt@gmail.com>
2024-10-23 14:04:30 -0700

    Add basic test for anthy (#16)
    Prepare for investigating #15 issue

---
+++ test/testanthy.cpp
@@ -0,0 +1,67 @@
+/*
+ * SPDX-FileCopyrightText: 2021-2021 CSSlayer <wengxt@gmail.com>
+ *
+ * SPDX-License-Identifier: GPL-2.0-or-later
+ *
+ */
+#include "testdir.h"
+#include "testfrontend_public.h"
+#include <fcitx-utils/eventdispatcher.h>
+#include <fcitx-utils/log.h>
+#include <fcitx-utils/standardpath.h>
+#include <fcitx-utils/testing.h>
+#include <fcitx/addonmanager.h>
+#include <fcitx/inputmethodmanager.h>
+#include <fcitx/instance.h>
+#include <iostream>
+
+using namespace fcitx;
+
+void scheduleEvent(EventDispatcher *dispatcher, Instance *instance) {
+    dispatcher->schedule([dispatcher, instance]() {
+        auto *anthy = instance->addonManager().addon("anthy", true);
+        FCITX_ASSERT(anthy);
+
+        auto defaultGroup = instance->inputMethodManager().currentGroup();
+        defaultGroup.inputMethodList().clear();
+        defaultGroup.inputMethodList().push_back(InputMethodGroupItem("anthy"));
+        defaultGroup.setDefaultInputMethod("");
+        instance->inputMethodManager().setGroup(defaultGroup);
+        auto *testfrontend = instance->addonManager().addon("testfrontend");
+        auto uuid =
+            testfrontend->call<ITestFrontend::createInputContext>("testapp");
+        auto ic = instance->inputContextManager().findByUUID(uuid);
+
+        RawConfig config;
+        config.setValueByPath("General/TypingMethod", "Nicola");
+        anthy->setConfig(config);
+
+        testfrontend->call<ITestFrontend::keyEvent>(uuid, Key("Muhenkan"),
+                                                    false);
+        testfrontend->call<ITestFrontend::keyEvent>(uuid, Key("a"), false);
+        testfrontend->call<ITestFrontend::keyEvent>(uuid, Key("a"), true);
+        testfrontend->call<ITestFrontend::keyEvent>(uuid, Key("Muhenkan"),
+                                                    true);
+
+        instance->exit();
+    });
+}
+
+int main() {
+    setupTestingEnvironment(TESTING_BINARY_DIR, {TESTING_BINARY_DIR "/src"},
+                            {TESTING_BINARY_DIR "/test"});
+    // fcitx::Log::setLogRule("default=5,table=5,libime-table=5");
+    char arg0[] = "testanthy";
+    char arg1[] = "--disable=all";
+    char arg2[] = "--enable=testim,testfrontend,anthy,testui";
+    char *argv[] = {arg0, arg1, arg2};
+    fcitx::Log::setLogRule("default=5,anthy=5");
+    Instance instance(FCITX_ARRAY_SIZE(argv), argv);
+    instance.addonManager().registerDefaultLoader(nullptr);
+    EventDispatcher dispatcher;
+    dispatcher.attach(&instance.eventLoop());
+    scheduleEvent(&dispatcher, &instance);
+    instance.exec();
+
+    return 0;
+}
